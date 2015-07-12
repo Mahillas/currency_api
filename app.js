e13c00f079d327ca967b06f79451776db24eb1d9
Duma Mtungwa <dumakude@gmail.com>
2015-07-12 19:41:44 +0200

    refactored

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var koa = require('koa');
 var Trade = require('./models/trades').Trade;
 var Q = require('q');
 var logger = require('winston');
+var cassandra = require('cassandra-driver');
+var co = require('co');
+var Client = require('co-cassandra')(require('cassandra-driver').Client);
+var client = new Client({contactPoints: ['localhost']});
 var app = koa();
 app.use(bodyParser());
 
@@ -37,16 +41,23 @@ var api = {
       this.body = 'Trade successfully created';
       this.status = 201;
     } catch (e) {
-      logger.error("Could not add trade via the API:" + e.message)
+      logger.error("Could not add trade via the API:" + e.message);
       this.message = e.message;
       this.status = 400;
     }
+  },
+
+  cass: function *(){
+    var query = 'select key from system.local';
+    var result =  yield client.execute(query);
+    this.body = result.rows;
   }
 };
 
 app.use(_.get('/trade', api.list));
 app.use(_.post('/trade', api.add));
 app.use(_.get('/trade/:id', api.show));
+app.use(_.get('/cass', api.cass));
 
 app.listen(3000);
 
